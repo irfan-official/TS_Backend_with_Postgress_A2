18302234f3aa0d572d7acddc205280e1437058a6
irfan-official <irfanofficial.work@gmail.com>
2025-12-08 12:52:15 +0600

    clean up user routes

--- src/module/user/user.routes.ts
+++ src/module/user/user.routes.ts
@@ -10,18 +10,6 @@ import authorizeMiddleware from "../../middleware/role.m";
 
 const router = express.Router();
 
-// router.get("/users", getUsers);
-
-// router.get("/users/:id", getUserById);
-
-// router.post("/users", createUser);
-
-// router.patch("/users/:id", updateUser);
-
-// router.delete("/users/:id", deleteUser);
-
-// latest ----------------------------------------->..
-
 router.get("/", authorizeMiddleware, getUsers); // admin only
 
 router.get("/:userId", getUserDetails); // both
